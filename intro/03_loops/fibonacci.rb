3c2de031c254b5daa19f5e3f8929c25d438392ca
targrox <dima-ano0@yandex.ru>
2022-02-14 00:05:25 +0300

    finish fibonacci.rb

--- intro/03_loops/fibonacci.rb
+++ intro/03_loops/fibonacci.rb
@@ -1,4 +1,12 @@
 # Print out all the Fibonacci numbers from 1 to 10 in order
 
 # Your code goes here
+x = Array.new
+x[0] = 1 
+x[1] = 1
 
+
+(2..9).each do |i|
+    x[i] = x[i-1] + x[i-2]
+end
+p x
